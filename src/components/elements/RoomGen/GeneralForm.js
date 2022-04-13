b3d582ef3152239cb1db4d8bb72dd1657f077f88
Mmx233 <admin@mmxblog.com>
2022-04-13 12:21:00 +0800

    feat: 常规页输入

--- src/components/elements/RoomGen/GeneralForm.js
+++ src/components/elements/RoomGen/GeneralForm.js
@@ -1,17 +1,27 @@
 import react from "react";
-import {Grid,TextField} from "@mui/material";
+import {Grid,TextField,MenuItem} from "@mui/material";
 
 import Switch from "../switch";
 
 export default class GeneralForm extends react.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleSwitchChange = this.handleSwitchChange.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
     handleSwitchChange(index,value){
         this.props.handleChange("switches",index,value);
     }
+    handleInputChange(index,value){
+        this.props.handleChange("inputs",index,value);
+    }
     render(){
-        return <Grid
+        return <react.Fragment><Grid
             container
             rowSpacing={2}
             flexShrink={1}
+            marginBottom={"2em"}
         >
             <Grid item xs={12}>
                 <TextField
@@ -34,5 +44,32 @@ export default class GeneralForm extends react.Component {
                 </Grid>
             })}
         </Grid>
+        <Grid
+            container
+            rowSpacing={3}
+            flexShrink={1}
+        >
+            {this.props.data.inputs.map((s,i)=>{
+                return <Grid
+                    key={s.label}
+                    item xs={12} sm={6}
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                    }}
+                >
+                    <TextField
+                        {...s}
+                        onChange={(e)=>this.handleInputChange(i,e.target.value)}
+                    >
+                        {s.selection?s.selection.map((o)=>{
+                            return <MenuItem key={o.label} value={o.value}>
+                                {o.label}
+                            </MenuItem>
+                        }):null}
+                    </TextField>
+                </Grid>
+            })}
+        </Grid>
+        </react.Fragment>
     }
 }
--- src/pages/room-gen.js
+++ src/pages/room-gen.js
@@ -10,23 +10,38 @@ export default class RoomGen extends react.Component {
         super(props);
         this.state={}
         //表单们
-        this.state.General={
+        this.state.General= {
             main: [
-                {label:"房间ID",value:1},
+                {label: "房间ID", value: 1,min:1, disabled: true},
             ],
             switches: [
-                {label:"显示消息弹幕",value:false,disabled:true},
-                {label:"显示醒目留言(SC)",value:false,disabled:true},
-                {label:"显示新舰长",value:false,disabled:true},
-                {label:"显示礼物",value:false,disabled:true},
-                {label:"显示礼物信息",value:false,disabled:true},
-                {label:"弹幕居下",value:false,disabled:true},
-                {label:"底部显示SC固定栏",value:false,disabled:true},
-                {label:"合并相似弹幕",value:false,disabled:true},
-                {label:"合并礼物",value:false,disabled:true},
-                {label:"只显示翻译弹幕",value:false,disabled:true},
-            ]
+                {label: "显示消息弹幕", value: false, disabled: true},
+                {label: "显示醒目留言(SC)", value: false, disabled: true},
+                {label: "显示新舰长", value: false, disabled: true},
+                {label: "显示礼物", value: false, disabled: true},
+                {label: "显示礼物信息", value: false, disabled: true},
+                {label: "弹幕居下", value: false, disabled: true},
+                {label: "底部显示SC固定栏", value: false, disabled: true},
+                {label: "合并相似弹幕", value: false, disabled: true},
+                {label: "合并礼物", value: false, disabled: true},
+                {label: "只显示翻译弹幕", value: false, disabled: true},
+            ],
+            inputs: [
+                {label: "翻译弹幕首字符", value: "【"},
+                {label: "打赏弹幕最低显示价格(元)", value: 0,type:"number",inputProps:{min:0}},
+                {label: "打赏停驻栏最低显示价格(元)", value: 0.1,type:"number",inputProps:{min:0,step:0.1}},
+                {label: "最大弹幕数", value: 0,type:"number",inputProps:{min:0}},
+                {label: "预留淡出弹幕数", value: 3,type:"number",inputProps:{min:0}},
+                {label: "弹幕停留时长(为0时持续停留)", value: 0,type:"number",inputProps:{min:0}},
+                {label: "图片插入方式", value: "0",select:true,selection:[
+                        {label:"替换关键词(无需符号)",value:"0"},
+                        {label:"替换关键词(需符号)",value:"1"},
+                        {label:"在文字消息后添加",value:"2"},
+                    ]},
+                {label: "最大图片数", value: 2,type:"number",inputProps:{min:0}},
+            ],
         }
+
         //恢复表单记录
         for(let k in this.state){
             for(let e in this.state[k]) {
