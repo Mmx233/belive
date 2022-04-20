a0f8213e9a0f8fc639f08b203f37fda1f4f037d8
Mmx233 <admin@mmxblog.com>
2022-04-20 21:25:21 +0800

    feat: room载入选项

--- src/pages/room.js
+++ src/pages/room.js
@@ -7,25 +7,45 @@ export default class Room extends react.Component {
     constructor(props) {
         super(props);
         this.state={
-            Options:{
-                General,
-                Forbid,
-                Test
-            },
-            Settings:{
-                TestMode:Boolean(this.context.searchParams.get('test_mode')),
-            }
-        }
 
-        for(let k in this.state.Options){
-            for(let i=0;i<this.state.Options[k].length;i++){
-                let q=this.context.searchParams.get(this.state.Options[k][i].key)
-                console.log(q)
-            }
         }
     }
+    componentDidMount() {
+        //载入选项
+        this.setState({
+            Settings: {
+                TestMode:Boolean(this.context.searchParams.get('test_mode')),
+            }
+        })
+        let forms={
+            General,
+            Forbid,
+            Test
+        }
+        let Options={}
+        for(let k in forms){
+            for(let a in forms[k]){
+                for(let i=0;i<forms[k][a].length;i++){
+                    let key=forms[k][a][i].key
+                    let q=this.context.searchParams.get(key)
+                    if(q!==null){
+                        switch (typeof forms[k][a][i].value){
+                            case 'number':
+                                q=q*1
+                                if(isNaN(q))continue
+                                break
+                            default:
+                                break
+                        }
+                        Options[key]=q
+                    }
+                }
+            }
+        }
+        this.setState({Options})
+    }
     render(){
-
+        return <div/>
     }
 }
 
